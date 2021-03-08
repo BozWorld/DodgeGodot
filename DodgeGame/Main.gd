f2d6a7095f0b6e4ce8c43590d65d4104358907d2
MoevaMondoha <mondohamoeva@hotmail.fr>
2021-03-08 07:03:45 +0100

    Add main Scene

---
+++ DodgeGame/Main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+func _ready():
+	randomize()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
